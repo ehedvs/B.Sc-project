03a4d84c91da3543f3b19a224833d410b6cb5b63
ehedvs <ehedvs2021@gmail.com>
2021-08-17 05:24:55 -0700

    student db modification

--- graduates/models.py
+++ graduates/models.py
@@ -11,6 +11,8 @@ from accounts.models import User
 from django.contrib.auth import get_user_model
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from registrar_admin.models import Faculty, Program
+
 
 
 #from ehedvs import settings
@@ -21,16 +23,16 @@ from django.dispatch import receiver
 class Student(models.Model):
     institution = models.ForeignKey(University, on_delete=models.CASCADE, null=True)
     id = models.CharField(max_length=200, primary_key=True, verbose_name='ID Number ')
-    first_name = models.CharField(max_length=200, verbose_name='first name')
-    middle_name = models.CharField(max_length=200, verbose_name='middle name')
-    last_name =  models.CharField(max_length=200, verbose_name='last name')
+    full_name = models.CharField(max_length=255, verbose_name='student name')
     gender = models.CharField(max_length=50)
     age = models.PositiveSmallIntegerField()
     registration_year = models.DateField(auto_now_add=True, null=True, verbose_name='Registration_Date')
     created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    school = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True, blank=True)
+    department=models.ForeignKey(Program, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.first_name + " " + self.middle_name + " "+self.last_name
+        return self.full_name
 
     
    
@@ -79,7 +81,7 @@ class Profile(models.Model):
     image = models.ImageField(default ='default.png', upload_to='student_image' )
 
     def __str__(self):
-        return f'{self.student.first_name} Profile'
+        return f'{self.student} Profile'
    
 
 #pre_save signal for generating  qr_code
